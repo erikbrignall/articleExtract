1b4830aafad7739a45de6b2b465689218611aa1f
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2023-09-28 11:33:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,27 @@ import streamlit as st
 openai.api_key = st.secrets["OpenAIapikey"]
 GPapikey = st.secrets["GPapikey"]
 
+###### FUNCTION EXTRACT VALUES FROM JSON
+## The below function loops through the JSON structure and returns any value matching the key
+def extract_values(obj, key):
+        """Pull all values of specified key from nested JSON."""
+        arr = []
+
+        def extract(obj, arr, key):
+            """Recursively search for values of key in JSON tree."""
+            if isinstance(obj, dict):
+                for k, v in obj.items():
+                    if isinstance(v, (dict, list)):
+                        extract(v, arr, key)
+                    elif k == key:
+                        arr.append(v)
+            elif isinstance(obj, list):
+                for item in obj:
+                    extract(item, arr, key)
+            return arr
+
+        results = extract(obj, arr, key)
+        return results
 
 ###### FUNCTION TO EXTRACT ELEMENTS FROM JSON RESPONSE AND FIND LATLONG
 def extractjson(obj):
